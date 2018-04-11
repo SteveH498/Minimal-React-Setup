89c01edd03a350929dc7f4dc28abacb630524bd7
Haas <stefan.haas02@sap.com>
2018-04-11 13:25:54 +0200

    Add initial minimal react dev setup

---
+++ README.txt
@@ -0,0 +1,7 @@
+# Install dependencies defined in package.json
+
+npm install
+
+# Build project
+
+npm run-script build
---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+    entry:{
+		reactcomponents: [
+			'./src/components/react-component1.js',
+			'./src/components/react-component2.js'
+		]
+	},
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: [{
+            test: /\.js$/,
+            exclude: /(node_modules)/,
+            use: {
+                loader: 'babel-loader'
+            }
+        }]
+    },
+    plugins: [
+        new HtmlWebpackPlugin(
+		{
+			template: './src/index.html',
+			filename: './index.html'
+		})
+    ]
+};
